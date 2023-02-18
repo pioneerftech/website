f333a86ca36976224f9d3797669c53fc3c50aef9
Danny <pasaydandg@gmail.com>
2023-02-19 01:49:41 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="home-page-container">
-      <div className='heading'>
-        Home
+      <div className='image-container'>
+        <div className='hero-content f-d-col d-f-c'>
+          <div className='heading'>Our Helping To The World</div>
+          <div className='subheading'>Our Helping To The World</div>
+        </div>
       </div>
     </div>
   )
