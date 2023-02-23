61254873346a4fc8fac36b125fc2f2c3ba5c638a
skylineent <skyline.enterprises55@gmail.com>
2023-02-23 23:45:29 +0530

    change about gallery, btn shadow, card color

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -12,6 +12,22 @@ import cardimg7 from "../../public/blog/cardimg7.jpg";
 import cardimg8 from "../../public/blog/cardimg8.jpg";
 
 function Events() {
+  function BiMessageRoundedDetail() {
+    return (
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth={0}
+        viewBox="0 0 24 24"
+        height="1em"
+        width="1em"
+      >
+        <path d="M12,2C6.486,2,2,5.589,2,10c0,2.908,1.898,5.515,5,6.934V22l5.34-4.005C17.697,17.852,22,14.32,22,10 C22,5.589,17.514,2,12,2z M12,16h-0.333L9,18v-2.417l-0.641-0.247C5.67,14.301,4,12.256,4,10c0-3.309,3.589-6,8-6s8,2.691,8,6 S16.411,16,12,16z" />
+        <path d="M7 7H17V9H7zM7 11H14V13H7z" />
+      </svg>
+    );
+  }
+
   return (
     <div className="events-container">
       <div className="image-container">
@@ -46,7 +62,12 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
         </div>
         <div className="blog-card hover">
@@ -70,9 +91,14 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
-        </div>{" "}
+        </div>
         <div className="blog-card hover">
           <div className="blog-image-container">
             <div className="blog-image-wrap">
@@ -94,9 +120,14 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
-        </div>{" "}
+        </div>
         <div className="blog-card hover">
           <div className="blog-image-container">
             <div className="blog-image-wrap">
@@ -118,9 +149,14 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
-        </div>{" "}
+        </div>
         <div className="blog-card hover">
           <div className="blog-image-container">
             <div className="blog-image-wrap">
@@ -142,9 +178,14 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
-        </div>{" "}
+        </div>
         <div className="blog-card hover">
           <div className="blog-image-container">
             <div className="blog-image-wrap">
@@ -166,7 +207,12 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
         </div>
         <div className="blog-card hover">
@@ -190,7 +236,12 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
         </div>
         <div className="blog-card hover">
@@ -217,7 +268,12 @@ function Events() {
             </div>
           </div>
           <div className="event-btn-wraper">
-            <div className="event-btn">Event Details</div>
+            <div className="event-btn">
+              Event Details
+              <span>
+                <BiMessageRoundedDetail />
+              </span>
+            </div>
           </div>
         </div>
       </div>
