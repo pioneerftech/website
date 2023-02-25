a6662a2acca12b3041b4fbb1ccb8c6b10765dc44
skylineent <skyline.enterprises55@gmail.com>
2023-02-25 23:32:52 +0530

    changes in contact us form

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -48,6 +48,22 @@ function Contactus() {
     );
   }
 
+  function AiOutlineSend() {
+    return (
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth={0}
+        viewBox="0 0 1024 1024"
+        height="1em"
+        width="1em"
+      >
+        <defs />
+        <path d="M931.4 498.9L94.9 79.5c-3.4-1.7-7.3-2.1-11-1.2-8.5 2.1-13.8 10.7-11.7 19.3l86.2 352.2c1.3 5.3 5.2 9.6 10.4 11.3l147.7 50.7-147.6 50.7c-5.2 1.8-9.1 6-10.3 11.3L72.2 926.5c-0.9 3.7-0.5 7.6 1.2 10.9 3.9 7.9 13.5 11.1 21.5 7.2l836.5-417c3.1-1.5 5.6-4.1 7.2-7.1 3.9-8 0.7-17.6-7.2-21.6zM170.8 826.3l50.3-205.6 295.2-101.3c2.3-0.8 4.2-2.6 5-5 1.4-4.2-0.8-8.7-5-10.2L221.1 403 171 198.2l628 314.9-628.2 313.2z" />
+      </svg>
+    );
+  }
+
   return (
     <div className="contactus-container">
       <div className="image-container">
@@ -156,12 +172,14 @@ function Contactus() {
       <PageSaperator />
       <div className="coutactus-form-container">
         <div className="contactus-form-sub-container">
-          <div className="subheading">Contact Us</div>
-          <div className="heading">Get In Touch With Us</div>
-          <div className="content">
-            We welcome your interest in our work! Please feel free to contact us
-            at Pioneer to learn more about our programs and how you can support
-            our mission.
+          <div className="contactus-details-wrap">
+            <div className="subheading">Contact Us</div>
+            <div className="heading">Get In Touch With Us</div>
+            <div className="content">
+              We welcome your interest in our work! Please feel free to contact
+              us at Pioneer to learn more about our programs and how you can
+              support our mission.
+            </div>
           </div>
           <div className="contactus-info-wrap">
             <div className="contactus-info-row">
@@ -201,7 +219,173 @@ function Contactus() {
             </div>
           </div>
         </div>
-        <div className="contactus-form-sub-container"></div>
+        <div className="contactus-form-sub-container">
+          <div className="contactus-form-wrap">
+            <div className="contactus-form">
+              <form>
+                <p className="name">
+                  <input
+                    name="name"
+                    type="text"
+                    className="validate[required,custom[onlyLetter],length[0,100]] feedback-input"
+                    placeholder="Name"
+                    id="name"
+                  />
+                </p>
+
+                <p className="Number">
+                  <input
+                    name="Number"
+                    type="text"
+                    className="validate[required,custom[Number]] feedback-input"
+                    id="Number"
+                    placeholder="Number"
+                  />
+                </p>
+                <p className="email">
+                  <input
+                    name="email"
+                    type="text"
+                    className="validate[required,custom[email]] feedback-input"
+                    id="email"
+                    placeholder="Email"
+                  />
+                </p>
+
+                <p className="text">
+                  <textarea
+                    name="text"
+                    className="validate[required,length[6,300]] feedback-input"
+                    id="comment"
+                    placeholder="Comment"
+                  ></textarea>
+                </p>
+
+                <div className="submit" id="button-blue">
+                  Send
+                  <AiOutlineSend />
+                  <div className="ease"></div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="span-top-container">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="span-bottom-container">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ app/pdp/pdp.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./pdp.scss";
+
+export default function pdp() {
+  return (
+    <div className="pdp-container">
+      <div className="image-container">
+        <div className="hero-content f-d-col d-f-c">
+          <div className="heading">Our Helping To The World</div>
+          <div className="subheading">
+            Empowered women and girls can lift their entire families and
+            communities out of poverty.
+          </div>
+        </div>
+      </div>
+      <div className="personalloan-close"></div>
+    </div>
+  );
+}
+
+// export default pdp;
