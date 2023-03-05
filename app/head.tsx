f1210b5c125f89c55eece558339e9ec9161fa5ef
Danny <pasaydandg@gmail.com>
2023-03-05 13:22:59 +0530

    changes for manifest2

--- app/head.tsx
+++ app/head.tsx
@@ -5,9 +5,9 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="title" content="Pioneer Foundation" />
       <meta name="description" content="Promote education agriculture, rural and youth development welfare of child, women and family to promote charity, social reforms of useful knowledge without any discrimination ofcaste, creed, language" />
-      <meta property="og:image" content="/logo-resized.png" key="ogimage" />
-      <meta property="image" content="/logo-resized.png" />
-      <meta name="twitter:image" content="/logo-resized.png" key="twitterimage" />
+      <meta property="og:image" content="https://pioneer-foundation.org/logo-resized.png" key="ogimage" />
+      <meta property="image" content="https://pioneer-foundation.org/logo-resized.png" />
+      <meta name="twitter:image" content="https://pioneer-foundation.org/logo-resized.png" key="twitterimage" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
