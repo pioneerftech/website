3a7a1b9a68331be246836a2558bb38b21dfe0746
Danny <pasaydandg@gmail.com>
2023-03-05 12:56:42 +0530

    changes for manifest

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,11 @@ export default function Head() {
       <meta name="title" content="Pioneer Foundation" />
       <meta name="description" content="Promote education agriculture, rural and youth development welfare of child, women and family to promote charity, social reforms of useful knowledge without any discrimination ofcaste, creed, language" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <meta name="theme-color" content="#067cc3" />
+      <link rel="manifest" href="/manifest.json" />
     </>
   )
 }
